d9bc76e49c0d09c02c6af9ec3b671be013754b3d
Carl Chinatomby <Carl.Chinatomby@gmail.com>
2011-04-06 14:41:55 -0400

    implemented lzw encoding in dictcomp.py

--- arithcomp.py
+++ arithcomp.py
@@ -6,6 +6,7 @@ characters and 10 digits. As new characters present the
 domains assigned to the symbols are changed accordingly.
 """
 import sys
+from math import floor, trunc
 
 class ArithComp():
 	"""
@@ -20,11 +21,12 @@ class ArithComp():
 		self.charset = {}
 		self.ranges = {}
 		self.prob = {}
-		self.totalchars = 0
-		self.initfreq = 0
+		self.cumfreq = {}
+		self.initfreq = 1
 		self.code = 0.0
 		self.decdata = ''
 		
+
 		#create initial frequency of digits
 		for number in xrange(0, 9+1):
 			self.charset[number] = self.initfreq
@@ -32,21 +34,34 @@ class ArithComp():
 		for letter in xrange(ord('a'), ord('z')+1):
 			self.charset[chr(letter)] = self.initfreq
 		self.charsetlen = len(self.charset)
-		
+		self.totalchars = self.charsetlen
+
 		#calculate ranges and probabiities
 		prev_high = 0
+		curcumfreq = self.initfreq		
 		for char in self.charset:
-			self.prob[char] = self.charset[char]/self.charsetlen
+			self.prob[char] = self.charset[char]/float(self.charsetlen)
 			self.ranges[char] = [prev_high, prev_high+self.prob[char]]
 			prev_high = prev_high + self.prob[char]
+			self.cumfreq[char] = curcumfreq 
+			curcumfreq += self.initfreq
 		#put try/catch statement here 
 		self.data = open(self.filename).read()
+		
+		self.initcharset = self.charset
+		self.initranges = self.ranges
+		self.initprob = self.prob
+		self.initcumfreq = self.cumfreq
+		self.initcharsetlen = self.charsetlen
+		self.inittotalchars = self.totalchars
 	
 	def print_table(self):
-		print "Char, Frequency, Probability, Range"
+		print "Char, Frequency, Probability, Range, CumFreq"
 		for char in self.charset:
-			print str(char) + ', \t' + str(self.charset[char]) + ', \t' + \
-			str(self.prob[char]) + ', \t' + str(self.ranges[char])
+			if self.charset[char]:
+				print str(char) + ', \t' + str(self.charset[char]) + ', \t' + \
+				str(self.prob[char]) + ', \t' + str(self.ranges[char]) + ' \t' + \
+				str(self.cumfreq[char])
 	
 	def print_prob_sum(self):
 		sum = 0
@@ -66,11 +81,35 @@ class ArithComp():
 		
 		#update ranges
 		prev_high = 0
+		curcumfreq = 0
 		for char in self.charset:		
 			self.prob[char] = float(self.charset[char])/self.totalchars
 			self.ranges[char] = [prev_high, prev_high + self.prob[char]]
+			self.cumfreq[char] = curcumfreq + self.charset[char]
+			curcumfreq = self.cumfreq[char]
 			prev_high = self.ranges[char][0] + self.prob[char]
 
+			
+	def add_initchar(self, c):
+		self.inittotalchars += 1
+		
+		#update frequency table			
+		self.initcharset[c] += 1
+		
+		#update probabilities
+		self.initprob[c] = float(self.initcharset[c])/self.inittotalchars
+		
+		#update ranges
+		prev_high = 0
+		curcumfreq = 0
+		for char in self.initcharset:		
+			self.initprob[char] = float(self.initcharset[char])/self.inittotalchars
+			self.initranges[char] = [prev_high, prev_high + self.initprob[char]]
+			self.initcumfreq[char] = curcumfreq + self.initcharset[char]
+			curcumfreq = self.initcumfreq[char]
+			prev_high = self.initranges[char][0] + self.initprob[char]
+
+			
 	def remove_char(self, c):		
 		self.totalchars -= 1
 
@@ -96,24 +135,48 @@ class ArithComp():
 		"""
 		"""
 		low = 0
-		high = 1
+		#high = 1
+		high = 9999
 		for i, c in enumerate(self.data):	
-			range = high - low
+			#range = high - low
+			range = high - low +1
+			print "table before: "
+			self.print_table()
 			print "just read: " + str(c)
 			if c.isalpha():
-				self.add_char(c.lower())
-				high = low + range * self.ranges[c.lower()][1] 
-				low = low + range * self.ranges[c.lower()][0]
+				#low = low + range*self.cumfreq[c.lower()]/float(max(self.cumfreq.values()))
+				print "the cum freq for this char is" + str(self.cumfreq[c.lower()])
+				print "the max is " + str(float(max(self.cumfreq.values())))
+				print "the division is" + str(self.cumfreq[c.lower()]/float(max(self.cumfreq.values())))
+				print "the character used for high is " + chr(ord(c.lower())+1)
+				print "the total chars is" + str(float(max(self.cumfreq.values())))
+				#high= low + range*self.cumfreq[chr(ord(c.lower())+1)]/float((max(self.cumfreq.values())-1))
+				nextchar = self.getnextrange(self.ranges[c.lower()][1])
+				#print "the next cahr is " + str(nextchar)
+				#high = low + range*self.cumfreq[nextchar]/float((max(self.cumfreq.values()))) - 1 
+				low = low + trunc(range*self.cumfreq[c.lower()]/max(self.cumfreq.values()))
+				high = low + trunc(range*self.cumfreq[nextchar]/max(self.cumfreq.values())) - 1
+				print "the cum freq for the next char is " + str(self.cumfreq[chr(ord(c.lower())+1)])
+				#high = low + range * self.ranges[c.lower()][1] 
+				#low = low + range * self.ranges[c.lower()][0]
+				#high = low + range * self.ranges[c.lower()][1] - 1 
+				#low = low + range * self.ranges[c.lower()][0]
+				print "the high is : " + str(high)
+				print "the low is : " + str(low)
+				self.add_char(c.lower())	
 			else:
-				self.add_char(int(c))
 				high = low + range * self.ranges[int(c)][1] 
 				low = low + range * self.ranges[int(c)][0]
+				self.add_char(int(c))
 			self.code = low			
 			self.print_table()
 			self.print_prob_sum()
 			
 			print "The total chars is " + str(self.totalchars)
 			print "The Code is " + str(self.code)
+			print "the high is : " + str(high)
+			print "the low is : " + str(low)
+
 			raw_input("Press Any Key for next input")
 			
 		#write the code and the condensed symbol table to a file
@@ -122,20 +185,51 @@ class ArithComp():
 		
 		return self.code
 	
+	def getnextrange(self, prevhigh):
+		for c in self.ranges:
+			if self.ranges[c][0] == prevhigh:
+				return c
+		
+	
 	def adaptive_decode(self, code):
 		"""
 		"""
 		curcode = code
-		while (code > 0 and self.totalchars > 0):
-			#print "the curcode is " + str(curcode)				
-			for char in self.ranges:
-				if curcode > self.ranges[char][0] and curcode < self.ranges[char][1]:
-					#print char, 
-					range = self.ranges[char][1] - self.ranges[char][0]
-					curcode -= self.ranges[char][0]
+		#while (curcode > 0 and self.totalchars > 0):
+		#	#print "the curcode is " + str(curcode)				
+		#	for char in self.ranges:
+		#		if curcode > self.ranges[char][0] and curcode < self.ranges[char][1]:
+		#			#print char
+		#			#self.print_table()
+		#			#self.print_prob_sum()
+		#			range = self.ranges[char][1] - self.ranges[char][0]
+		#			curcode -= self.ranges[char][0]
+		#			curcode /= range
+		#			self.remove_char(char)
+		#			self.decdata += str(char)
+		count = 36
+		#while (curcode >0 and curcode < 1.0):
+		while (curcode > 0 and count < self.totalchars):
+			print "the curcode is " + str(curcode)				
+			for char in self.initranges:
+				if curcode > self.initranges[char][0] and curcode < self.initranges[char][1]:
+					print char
+					self.print_table()					
+					#self.print_prob_sum()
+					range = self.initranges[char][1] - self.initranges[char][0]
+					print "range is " + str(range)
+					curcode -= self.initranges[char][0]
+					print "curcode is now" + str(curcode)
 					curcode /= range
-					self.remove_char(char)
-					self.decdata = char + self.decdata
+					self.add_initchar(char)
+					self.decdata += str(char)
+					count += 1
+					print "the curcode is: " + str(curcode)
+					raw_input("waiting")
+			
+
+		
+		
 		self.code = 0
 		print "The decoded data is " + self.decdata
 
@@ -145,6 +239,7 @@ def main():
 	#filename = sys.argv[1]
 	filename = "data/arithtest.txt"
 	comp = ArithComp(filename)
+	comp.print_table()
 	code = comp.adaptive_encode()
 	comp.adaptive_decode(code)
 	
--- dictcomp.py
+++ dictcomp.py
@@ -1,29 +1,55 @@
 """
-Implements the dictionary based compression algorithm
+Implements the LZW dictionary based compression algorithm
 """
+import sys
 
 class DictComp():
 	"""
 	"""
-	def __init__(self):
+	def __init__(self, filename):
 		"""
 		"""
-		pass
+		self.table = {}
+		self.tablesize = 255
+		self.data = open(filename, 'r').read()
+		self.encdata = ''
+		self.decdata = ''
+		for val in xrange(self.tablesize+1):
+			self.table[chr(val)] = val
 	
+	def print_table(self):
+		print "Name, Values"
+		for c in self.table:
+			print c + ', ' + str(self.table[c])
+			
 	def compress(self):
 		"""
 		"""
-		pass
+		print "Starting Compression of " + str(self.data)
+		newcode  = ''
+		for i, c in enumerate(self.data):
+			if newcode+c in self.table:
+				newcode += c
+			else:
+				print str(newcode) + " became " + str(self.table[newcode])
+				self.encdata += str(self.table[newcode])
+				self.table[newcode+c] = max(self.table.values())+1
+				newcode = c
+		
+		return self.encdata
 	
-	def decompress(self):
+	def decompress(self, code):
 		"""
 		"""
-		pass
+		
 	
 def main():
 	"""
 	"""
-	pass
+	filename = "data/lzwtest.txt"
+	comp = DictComp(filename)
+	code = comp.compress()
+	comp.decompress(code)	
 
 if __name__=="__main__":
 	main()
